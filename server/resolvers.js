ce91fe2e663020c48938c95ea75f7fa63f3b0d73
ktayyeb <khaledwaleed180@gmail.com>
2022-10-19 01:19:29 +0200

    Initial commit

---
+++ server/resolvers.js
@@ -0,0 +1,22 @@
+const castData= require("./dummy.json").Cast;
+const moviesData= require("./dummy.json").Movies;
+const test=require('./test.json');
+
+const resolvers = {
+    Query: {
+      Cast() {
+        return castData;
+      },
+    },
+
+    Query: {
+        Movies() {
+          return moviesData;
+        },
+      },
+      
+  
+
+  };
+
+  module.exports = resolvers;
---
+++ server/schema.js
@@ -0,0 +1,56 @@
+const { gql } = require('apollo-server');
+
+//Movies: [Movie!]!
+//movies:[Movie]
+const typeDefs=gql`
+type Query {
+    Cast: [Member]
+    Movies: [Movie]
+}
+
+type Member {
+    "The id of the cast member"
+    id:ID!
+
+    "The cast member's name"
+    name: String!
+
+    "The cast member's birthdate"
+    birthdate: String!
+
+    "The cast member's nationality"
+    nation:String!
+
+    "The movies that he/she played a role in"
+    movies:[Movie]
+}
+
+
+"A track is a group of Modules that teaches about a specific topic"
+type Movie{
+    "The unique id for the movie"
+    id: ID!
+
+    "The title of the movie"
+    title: String!
+
+    "The list of names of the cast members"
+    cast: [String!]
+
+    "year of release"
+    year: Int
+
+    "The runtime of the movie in minutes"
+    length: Int
+
+    "Movie's Rating"
+    rating: Float
+    
+}
+`;
+
+
+module.exports = typeDefs;
+
+
+
